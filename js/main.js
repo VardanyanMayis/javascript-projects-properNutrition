0ad4ccfe1ead24979cd2556e34dfe104c6fca027
Mayis Vardanyan <crftlf@gmail.com>
2023-04-16 18:56:59 +0400

    Add tabs (in js file) for project

---
+++ js/main.js
@@ -0,0 +1,43 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', () => {
+
+	const tabs = document.querySelectorAll('.tabheader__item'),
+		tabsParent = document.querySelector('.tabheader__items'),
+		tabsComntent = document.querySelectorAll('.tabcontent');
+
+
+	function hiddenTabsContent() {
+		tabsComntent.forEach(item => {
+			item.classList.remove('show', 'fade');
+			item.classList.add('hidden');
+		});
+
+		tabs.forEach(item => {
+			item.classList.remove('tabheader__item_active');
+		});
+	}
+
+	function showTabContent(index = 0) {
+		tabsComntent[index].classList.remove('hidden');
+		tabsComntent[index].classList.add('show', 'fade');
+
+		tabs[index].classList.add('tabheader__item_active');
+	}
+
+	tabsParent.addEventListener('click', (event) => {
+		const target = event.target;
+        
+		if(target && target.classList.contains('tabheader__item')) {
+			tabs.forEach((item, index) => {
+				if(item == target) {
+					hiddenTabsContent();
+					showTabContent(index);
+				}
+			});
+		}
+	});
+
+	hiddenTabsContent();
+	showTabContent();
+});
