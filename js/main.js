2bcbbf2cf216e85994338856520d55b0d934f511
Mayis Vardanyan <crftlf@gmail.com>
2023-05-08 15:46:39 +0400

    Add calc for kkal

--- js/main.js
+++ js/main.js
@@ -358,6 +358,13 @@ window.addEventListener('DOMContentLoaded', () => {
 	});
 
 	function setCurrect() {
+		if(slidIndex > slayderOffers.length) {
+			slidIndex = 1;
+		}
+		if(slidIndex == 0) {
+			slidIndex = slayderOffers.length;
+		}
+		slideOffset = parseInt(width) * slidIndex;
 		currectSlaidNumber.textContent = setZero(slidIndex);
 	}
 
@@ -376,23 +383,174 @@ window.addEventListener('DOMContentLoaded', () => {
 	
 	const dotItems = dotsBlock.querySelectorAll('div');
 	function moveToDoteIndex(event) {
-		console.log('click');
 		let index = +event.target.dataset.dot;
 		delateSelectedDot();
 		slidIndex = index + 1;
 		setCurrect();
 		event.target.classList.add('secected');
 		slaidFiled.style.transform = `translateX(-${index * parseInt(width)}px)`;
-		console.log(index);
 	}
 
 	function delateSelectedDot() {
-		console.log(dotItems);
-		dotItems[slidIndex - 1].classList.remove('secected');
+		dotItems?.[slidIndex - 1].classList.remove('secected');
 	}
 
 	function addSelectingDot() {
-		dotItems[slidIndex - 1].classList.add('secected');
+		dotItems?.[slidIndex - 1].classList.add('secected');
 	}
 
+	// End Slayder
+
+	// KKal Calc
+
+	const result = document.querySelector('.calculating__result span');
+	let weigth, heigth, age, gender, BMR;
+
+	getStorageValues();
+	showStorageValues('#gender');
+	showStorageValues('.calculating__choose_big');
+	totalKKal();
+	GetDynamicValues();
+	GetStaticValues('#gender', 'calculating__choose-item_active');
+	GetStaticValues('.calculating__choose_big', 'calculating__choose-item_active');
+
+	function getStorageValues() {
+		if(localStorage.getItem('gender')) {
+			gender = localStorage.getItem('gender');
+		} else {
+			gender = document.querySelector('#gender .calculating__choose-item_active').id;
+		}
+			
+		if(localStorage.getItem('BMR')) {
+			BMR = localStorage.getItem('BMR');
+		} else {
+			BMR = document.querySelector('.calculating__choose_big .calculating__choose-item_active')
+				.dataset['bmr'];
+		}
+
+		if(localStorage.getItem('weigth')) {
+			weigth = localStorage.getItem('weigth');
+			document.querySelector('#weight').value = weigth;
+		}
+		if(localStorage.getItem('height')) {
+			heigth = localStorage.getItem('height');
+			document.querySelector('#height').value = heigth;
+		}
+		if(localStorage.getItem('weigth')) {
+			age = localStorage.getItem('age');
+			document.querySelector('#age').value = age;
+		}
+	}
+
+	function setStorageValues(key, value) {
+		localStorage.setItem(key, value);
+	}
+
+	function totalKKal() {
+		setStorageValues('gender', gender);
+		setStorageValues('BMR', BMR);
+
+		if(!(gender && BMR && weigth && heigth && age)) {
+			result.textContent = '____';
+			return;
+		}
+
+		let Kkal;
+		if(gender == 'female') {
+			Kkal = Math.round((447.6 + (9.2 * weigth) + 
+				(3.1 * heigth) - (4.3 * age)) * BMR);
+		} else {
+			Kkal = Math.round((88.36 + (13.4 * weigth) + 
+				(4.8 * heigth) - (5.7 * age)) * BMR);
+		}
+
+		if(Kkal) {
+			result.textContent = Kkal;
+		} else {
+			result.textContent = '____';
+		}
+		
+	}
+
+	function showStorageValues(parentElement) {
+		const valuesBtn = document.querySelectorAll(`${parentElement} div`);
+		valuesBtn.forEach(item => {
+			if(item.dataset['bmr']) {
+				if(item.dataset['bmr'] != BMR) {
+					item.classList.remove('calculating__choose-item_active');
+				} else {
+					item.classList.add('calculating__choose-item_active');
+				}
+			} else {
+				if(item.id != gender) {
+					item.classList.remove('calculating__choose-item_active');
+				} else {
+					item.classList.add('calculating__choose-item_active');
+				}
+			}
+
+		});
+	}
+
+	function GetStaticValues(parentElement, selectedClass) {
+		const valuesBtn = document.querySelectorAll(`${parentElement} div`);
+
+		valuesBtn.forEach(item => {
+			item.addEventListener('click', (event) => {
+				event.preventDefault();
+	
+				valuesBtn.forEach(item => {
+					item.classList.remove(selectedClass);
+				});
+				event.target.classList.add(selectedClass);
+
+				if(event.target.dataset['bmr']) {
+					BMR = event.target.dataset['bmr'];
+				} else {
+					gender = event.target.id;
+				}
+
+				totalKKal();
+			});
+		});
+	}
+
+	function GetDynamicValues() {
+		const valueInputs = document.querySelectorAll('.calculating__choose_medium input');
+		valueInputs.forEach(item => {
+			item.addEventListener('input', (event) => {
+				event.preventDefault();
+				let value = event.target.value;
+				let currect;
+
+				if(value.match(/\D/g)) {
+					event.target.style.border = '2px solid red';
+					event.target.style.color = 'red';
+					currect = false;
+					
+				} else {
+					event.target.style.border = 'none';
+					event.target.style.color = 'black';
+					currect = true;
+				}
+
+				switch(event.target.id) {
+				case 'height':
+					if(currect) setStorageValues('height', value);
+					heigth = value;
+					break;
+				case 'weight':
+					if(currect) setStorageValues('weigth', value);
+					weigth = value;
+					break;
+				case 'age':
+					if(currect) setStorageValues('age', value);
+					age = value;
+					break;
+				}
+
+				totalKKal();
+			});
+		});
+	}
 });
